6ac07a9ba37f8e6ed9a6fb7516d2d9aec91a9041
CONG.LEI01 <507452965@qq.com>
2019-07-15 15:40:56 +0800

    update

--- src/main/java/Exam2/ReturnByCountryID.java
+++ src/main/java/Exam2/ReturnByCountryID.java
@@ -7,16 +7,19 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class ReturnByCountryID {
-    @Test
+
     public  void ReturnCityId() throws Exception {
         CityEntity cityEntity = new CityEntity();
         Connection connection = JDBCTools.getConnection();
         String sql = "select distinct country ,city  from city ,country where city.country_id = country.country_id";
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
         ResultSet resultSet = preparedStatement.executeQuery();
+        System.out.print("Country(国家）"+"------");
+        System.out.println("City（城市）");
         while(resultSet.next()){
-            cityEntity.setCountry(resultSet.getString("country"));
-            cityEntity.setCity(resultSet.getString("city"));
+            /*cityEntity.setCountry(resultSet.getString("country"));
+            cityEntity.setCity(resultSet.getString("city"));*/
+            System.out.print(resultSet.getString("country")+"------");
             System.out.println(resultSet.getString("city"));
         }
     }
